5ca3cd7fe3bc215744bb9221b42f0f863e12ff74
allen <allen>
2020-07-24 23:55:33 +0800

    fix: 消除 ts 报错

--- test/parseQuery.test.ts
+++ test/parseQuery.test.ts
@@ -5,7 +5,8 @@ test('解析 key1=18127873881&key2=2', () => {
 });
 
 test('解析 https://jestjs.io/docs/zh-Hans/using-matchers?key1=18127873881&key2=2', () => {
-  expect(parseQuery('https://jestjs.io/docs/zh-Hans/using-matchers?key1=18127873881&key2=2')).toEqual({ 'https://jestjs.io/docs/zh-Hans/using-matchers?key1': '18127873881', key2: '2' });
+  expect(parseQuery('https://jestjs.io/docs/zh-Hans/using-matchers?key1=18127873881&key2=2'))
+    .toEqual({ 'https://jestjs.io/docs/zh-Hans/using-matchers?key1': '18127873881', 'key2': '2' });
 });
 
 test('解析空字符串', () => {
