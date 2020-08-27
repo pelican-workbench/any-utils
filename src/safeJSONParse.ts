983991c5a975694a3b3d5f70863a04a703e65018
allen <allen>
2020-08-27 10:11:55 +0800

    fix: 错别字修改

--- src/safeJSONParse.ts
+++ src/safeJSONParse.ts
@@ -1,10 +1,10 @@
 /**
  * 解析 JSON 字符串对象
  */
-export function safeJSONParse(str: string, defauValue: any = {}) {
+export function safeJSONParse(str: string, defaultValue: any = {}) {
   try {
     return JSON.parse(str);
   } catch (error) {
-    return defauValue;
+    return defaultValue;
   }
 }
